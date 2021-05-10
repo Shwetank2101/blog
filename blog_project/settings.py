4f8b355d40719d8b5b2231d913deafcb7ee052c2
Shwetank2101 <shwetankdixitofficial@gmail.com>
2021-05-11 03:13:49 +0530

    Heroku config

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # new!
 
 STATIC_URL = '/static/'
 
